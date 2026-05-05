292683231a764647076c2b94d0733f352581c2d4
Tan-Berbson <berbsontan01@gmail.com>
2026-05-05 13:13:40 +0800

    #Add Code for BaseController and LessonPlan controller

--- YAWA.COM/Controllers/BaseController.cs
+++ YAWA.COM/Controllers/BaseController.cs
@@ -1,11 +1,16 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace YAWA.COM.Controllers
 {
     [Authorize]
     public class BaseController : Controller
     {
-       
+        protected string CurrentUserId =>
+            User.FindFirstValue(ClaimTypes.NameIdentifier)
+            ?? throw new InvalidOperationException("User  not found.");
+
     }
+    
 }
--- YAWA.COM/Controllers/LessonPlanController.cs
+++ YAWA.COM/Controllers/LessonPlanController.cs
@@ -1,12 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using YAWA.COM.Contracts;
+using YAWA.COM.Data;
 
 namespace YAWA.COM.Controllers
 {
     public class LessonPlanController : BaseController
     {
-        public IActionResult Index()
+        private readonly IBaseRepository<LessonPlanner> _repo;
+
+        public LessonPlanController(IBaseRepository<LessonPlanner> repo)
         {
-            return View();
+            _repo = repo;
         }
+        public async Task<IActionResult> Index()
+        {
+          var lesson = await _repo.GetAll(CurrentUserId);
+            return View(lesson);
+        }
+        public async Task<IActionResult> Create(LessonPlanner lesson)
+        {
+            if(!ModelState.IsValid)
+                return View(lesson);
+
+            await _repo.Create(lesson, CurrentUserId);
+            TempData["Message"] = $"{lesson} Created Successfully";
+            return RedirectToAction("Index");
+        }
+
     }
 }
